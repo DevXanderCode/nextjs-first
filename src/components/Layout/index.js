aab4c287f510d9580f8cc6b540c47b8fd6396711
Alex Nweke <nwekealexchinedu@gmail.com>
2021-02-03 20:00:39 +0100

    created the Footer component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,10 +1,12 @@
 import Header from "../Header";
+import Footer from "../Footer";
 
 function Layout({ children, ...props }) {
   return (
     <div>
       <Header />
       {children}
+      <Footer />
     </div>
   );
 }
